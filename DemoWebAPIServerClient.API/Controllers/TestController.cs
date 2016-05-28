fcaccc6174250fb10e6b43eb9ce356ac9ccadb0e
Fabiano Alberto Nalin <fnalin@msn.com>
2016-05-28 16:00:56 -0300

    Configuring WebAPI project

---
+++ DemoWebAPIServerClient.API/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace DemoWebAPIServerClient.API.Controllers
+{
+    public class TestController:ApiController
+    {
+        public string Get() {
+            return "Funcionou";
+        }
+    }
+}
---
+++ DemoWebAPIServerClient.API/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Owin;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(DemoWebAPIServerClient.API.Startup))]
+
+namespace DemoWebAPIServerClient.API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            ConfigureWebApi(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        public static void ConfigureWebApi(HttpConfiguration config)
+        {
+            var formatters = config.Formatters;
+            formatters.Remove(formatters.XmlFormatter);
+
+            var jsonSettings = formatters.JsonFormatter.SerializerSettings;
+            jsonSettings.Formatting = Formatting.Indented;
+            jsonSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
